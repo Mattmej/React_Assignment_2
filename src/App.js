ce5692118f5070bcd72ecceacf792f225d5685fd
unknown <mattcmej@gmail.com>
2019-02-05 16:39:26 -0800

    Complete up to step 5 of README, but get error involving keys. Will edit code.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import LengthReader from './Components/LengthReader';
 import ValidationComponent from './Components/ValidationComponent';
+import CharComponent from './Components/CharComponent';
 
 class App extends Component {
 
   state = { 
     stringToRead: '',
-    stringLength: 0
+    stringLength: 0,
+    // id: Math.random() * 100
   };
 
   // Read the length of a string
@@ -25,10 +27,36 @@ class App extends Component {
       stringToRead: ourString
       
     });
+    // console.log(ourString)
 
   }
 
   render() {
+
+    let ourString = this.state.stringToRead;
+    console.log(ourString[0]);
+    let charArrayItem = null;
+    let charArray = [];
+
+
+    if (this.state.stringLength > 0) { 
+      for (let i=0; i<this.state.stringLength; i++) { 
+        charArrayItem = (
+          <div>
+            <CharComponent 
+              // key={}
+              character={ourString[i]}/>
+          </div>
+        );
+        charArray.push(charArrayItem);
+      }
+    }
+
+
+
+
+
+
     return (
       <div>
         <LengthReader 
@@ -36,6 +64,11 @@ class App extends Component {
           length={this.state.stringLength} />
 
         <ValidationComponent stringLength={this.state.stringLength} />
+
+        {charArray}
+
+
+
       </div>
      
     );
--- src/Components/CharComponent.js
+++ src/Components/CharComponent.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const charComponent = (props) => { 
+const charComponent = (props) => {
+    return ( 
+        <div>
+            <p>{props.character}</p>
+        </div>
+    )
     
-}
+};
+
+export default charComponent;
