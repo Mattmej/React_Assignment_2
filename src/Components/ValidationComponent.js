b79f52e7c0854d4c1273804d01bfae8e5bc75356
unknown <mattcmej@gmail.com>
2019-02-06 16:10:34 -0800

    Make ValidationComponent code more concise / elegant.

--- src/Components/ValidationComponent.js
+++ src/Components/ValidationComponent.js
@@ -2,18 +2,31 @@ import React from 'react';
 
 const validationComponent = (props) => { 
 
+/* === Less Elegant Method === */
+    // if (props.stringLength < 5) { 
+    //     return (
+    //         <p>Text too short!</p>
+    //     )
+    // }
+
+    // else { 
+    //     return (
+    //         <p>Text long enough.</p>
+    //     )
+    // }
+
+
+
+/* === More Elegant Method === */
+    let message = 'Text long enough.';
+
     if (props.stringLength < 5) { 
-        return (
-            <p>Text too short!</p>
-        )
+        message = 'Text too short!';
     }
 
-    else { 
-        return (
-            <p>Text long enough.</p>
-        )
-    }
-
+    return (
+        <p>{message}</p>
+    )
 }
 
 export default validationComponent;
